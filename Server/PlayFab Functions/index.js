f8e512fe36d23363812fb5d13034394b7463f1e5
Daxx <64298070+Daxxboii@users.noreply.github.com>
2022-11-18 23:45:35 +0530

    Added Playfab Funcs (Completed)

--- Server/PlayFab Functions/index.js
+++ Server/PlayFab Functions/index.js
@@ -5,27 +5,53 @@ var PlayFabServer = playfab.PlayFabServer
 PlayFabServer.settings.titleId = "E5D93";
 PlayFabServer.settings.developerSecretKey = "NGO8JU7YWDHH7EDS893B9KRDJEC4FAXICFF1WHG3GF8MAQNZU3";
 
-const ID = '6019B424DDF702FC'
+const ID = ''
 
 const express = require('express')
 const app = new express();
+const bodyParser = require("body-parser")
+const fs = require("fs");
 
 app.get('/', (req, res) => {
   res.send('Health Check')
 })
+app.use(bodyParser.json());
 
 app.listen(3000, () => {
   console.log(`Example app listening on port`);
 })
 
 //Adds a certain XP to the PlayerID
-async function UpdateXP(AmountOfXP,PlayerID){
-   var update = PlayFabServer.UpdatePlayerStatistics({PlayFabId : PlayerID,
-Statistics : [{StatisticName: "XP",Value : AmountOfXP},]
-});
+async function UpdateStat(StatName, AmountOfXP, PlayerID) {
+  var update = PlayFabServer.UpdatePlayerStatistics({
+    PlayFabId: PlayerID,
+    Statistics: [{ StatisticName: StatName, Value: AmountOfXP },]
+  });
 }
 
 //Called When A Player Creates an ID for the first Time
-app.get('/Initialize', (req, res) => {
-    
-  })
+app.post("/Initialize", (req, res) => {
+  ID = req.body[0].EntityId;
+
+  UpdateStat(XP, 0, ID)
+  UpdateStat(Level, 0, ID)
+
+  console.log("Player Initialized")
+})
+
+//Called When XP is Updated For a Player
+app.post("/XPReceived", (req, res) => {
+
+  ID = req.body[0].EntityId;
+
+  UpdateStat(XP, 0, ID)
+  UpdateStat(Level, 0, ID)
+
+  console.log("Player Initialized")
+})
+
+
+function CheckForLevelUP() {
+  //Perform Calculations 
+  // UpdateStat(Level, Whatever Level Player Is, ID)
+}
